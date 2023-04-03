37c512966df23666e32ab9bebc00840ea053e159
Server LTO <www-data@latoucheoriginale.fr>
2023-04-03 08:25:44 +0000

    Update paypal plugin hook

--- src/PluginHook/PaypalPluginHook.php
+++ src/PluginHook/PaypalPluginHook.php
@@ -15,11 +15,15 @@ final class PaypalPluginHook extends AbstractPluginHook
     {
         file_replace('sizeof($v) <= 0 && is_array($v)', 'is_array($v) && sizeof($v) <= 0', $this->getBundleDir().'/lib/PayPal/Common/PayPalModel.php');
         $this->Print('Updated "./lib/PayPal/Common/PayPalModel.php" file. `Check is_array($v)` first');
+        file_replace('$handlers = array()', '$handlers', $this->getBundleDir().'/lib/PayPal/Transport/PayPalRestCall.php');
+        $this->Print('Updated "./lib/PayPal/Transport/PayPalRestCall.php" file. `Optional parameter $handlers declaration` removed');
     }
 
     public function onPackageUpdate(PackageEvent $event)
     {
         file_replace('sizeof($v) <= 0 && is_array($v)', 'is_array($v) && sizeof($v) <= 0', $this->getBundleDir().'/lib/PayPal/Common/PayPalModel.php');
         $this->Print('Updated "./lib/PayPal/Common/PayPalModel.php" file. `Check is_array($v)` first');
+        file_replace('$handlers = array()', '$handlers', $this->getBundleDir().'/lib/PayPal/Transport/PayPalRestCall.php');
+        $this->Print('Updated "./lib/PayPal/Transport/PayPalRestCall.php" file. `Optional parameter $handlers declaration` removed');
     }
 }
