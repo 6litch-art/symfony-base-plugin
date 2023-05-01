d8b701356da87ef912d46930fcdff718ea85b2e4
Marco Meyer <marco.meyer@glitchr.io>
2023-05-01 19:15:11 +0900

    Code inspector

--- src/PluginHook/EasyAdminPluginHook.php
+++ src/PluginHook/EasyAdminPluginHook.php
@@ -68,7 +68,7 @@ final class EasyAdminPluginHook extends AbstractPluginHook
 
     public function enableMultiWordSearch()
     {
-        file_replace("'%'.\$lowercaseQuery.'%'", "'%'.str_replace(' ', '%', \$lowercaseQuery).'%'", $this->getBundleDir().'/src/Orm/EntityRepository.php');
+        file_replace("'%'.\$lowercaseQuery.'%'", "'%'.str_replace(' ', '%', \$lowercaseQuery).'%'", $this->getBundleDir() . '/src/Orm/EntityRepository.php');
         $this->Print('Updated `./Orm/EntityRepository.php` file. Allow multi word search in CRUD controllers.');
     }
 }
