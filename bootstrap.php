a02e240668e57f8268182668ba042c6c36b309a8
Marco Meyer-Conde <marco.meyerconde@gmail.com>
2025-04-13 22:23:54 +0900

    update plugin

--- bootstrap.php
+++ bootstrap.php
@@ -253,7 +253,7 @@ if (!class_exists("CodeModifier")) {
             
             // Collect the yielded content from the generator function
             $iterator = $fn($tag, $search, $subject, ...$args);
-            dump($tag, $search, $subject, $fn, $args);
+            print_r($tag, $search, $subject, $fn, $args);
             $generatedContent = $iterator ? iterator_to_array($iterator) : [];
 
             // If no modification has been made (i.e., the generator yields the same content)
