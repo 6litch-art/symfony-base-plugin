a1acd7658bc0f4e65e563f388f313c2837b33af5
Marco Meyer-Conde <marco.meyerconde@gmail.com>
2025-05-06 18:50:42 +0800

    implement FileLinkFormatter Hook

---
+++ src/Package/Hook/FileLinkFormatterPackageHook.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Base\Composer\Package\Hook;
+
+use Base\Composer\Package\AbstractHook;
+use Composer\Installer\PackageEvent;
+use Base\Composer\CodeModifier;
+
+/**
+ *
+ */
+final class ParameterBagPackageHook  extends AbstractHook
+{
+    public function getPackageName(): string
+    {
+        return 'symfony/error-handler';
+    }
+
+    public function getPackageRequirements(): string
+    {
+        return "*";
+    }
+
+    public function onPackageChange(PackageEvent $event)
+    {
+        $this->print('Remove @final in `FileLinkFormatter.php`.');
+        $codeModifier = new CodeModifier($this->getBundleDir() . '/ErrorRenderer/FileLinkFormatter.php', $this->getAuthor());
+        $codeModifier->replaceInComments("final", '@final', '');
+
+        $this->print('Remove @internal in `FileLinkFormatter.php`.');
+        $codeModifier = new CodeModifier($this->getBundleDir() . '/ErrorRenderer/FileLinkFormatter.php', $this->getAuthor());
+        $codeModifier->replaceInComments("internal", '@internal', '');
+
+        $this->print('Turn private into protected in `FileLinkFormatter.php`.');
+        $codeModifier = new CodeModifier($this->getBundleDir() . '/ErrorRenderer/FileLinkFormatter.php', $this->getAuthor());
+        $codeModifier->replace("private-protected", 'private', 'protected');
+    }
+
+    public function onPackageRemove(PackageEvent $event)
+    {
+        $codeModifier = new CodeModifier($this->getBundleDir() . '/ErrorRenderer/FileLinkFormatter.php', $this->getAuthor());
+        $codeModifier->restore();
+    }
+}
